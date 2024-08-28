be0f29d08038cf1617b8918f3c7a369a5df31d16
saeed rastegar <saeed.rastegarrazaghi@tu-clausthal.de>
2024-08-28 14:38:41 +0200

    initial commit for the game

---
+++ script.js
@@ -0,0 +1,53 @@
+'use strict';
+
+// These works exactly same, but getElementById is a little faster than querySelector
+
+// Selecting elements
+// Note: getElementById is a little faster than querySelector
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+// Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0; // Player one
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  // 1. Generating a random number
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  // 2. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  // 3. Check for rolled 1: if true, switch to next player
+  if (dice !== 1) {
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  // Add current score to active player score
+  scores[activePlayer] += currentScore;
+  document.getElementById(`current--${activePlayer}`).textContent = scores[activePlayer];.
+});
